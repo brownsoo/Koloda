16c483bf5dc1148eb4f722b544bb171ecaa66b8e
hyonsoo han <hyonsoo.han@tlx.co.kr>
2018-03-14 15:10:23 +0900

    Fixed deletion error in condition - visible cards count is not equal with countOfCards

--- Pod/Classes/KolodaView/KolodaView.swift
+++ Pod/Classes/KolodaView/KolodaView.swift
@@ -697,16 +697,22 @@ open class KolodaView: UIView, DraggableCardDelegate {
     
     // MARK: Cards managing - Deletion
     
-    private func proceedDeletionInRange(_ range: CountableClosedRange<Int>) {
+    private func proceedDeletionInRange(_ range: CountableClosedRange<Int>, animated: Bool = true) {
         let deletionIndexes = [Int](range)
         deletionIndexes.sorted { $0 > $1 }.forEach { deletionIndex in
             let visibleCardIndex = deletionIndex - currentCardIndex
-            let card = visibleCards[visibleCardIndex]
-            card.delegate = nil
-            card.swipe(.right) {
-                
+            if visibleCardIndex < visibleCards.count {
+                let card = visibleCards[visibleCardIndex]
+                card.delegate = nil
+                if animated {
+                    card.swipe(.right) {
+                        // removed card from superview
+                    }
+                } else {
+                    card.removeFromSuperview()
+                }
+                visibleCards.remove(at: visibleCardIndex)
             }
-            visibleCards.remove(at: visibleCardIndex)
         }
     }
     
@@ -720,7 +726,7 @@ open class KolodaView: UIView, DraggableCardDelegate {
         countOfCards = dataSource.kolodaNumberOfCards(self)
         let visibleIndexes = [Int](indexRange).filter { $0 >= currentCardIndex && $0 < currentCardIndex + countOfVisibleCards }
         if !visibleIndexes.isEmpty {
-            proceedDeletionInRange(visibleIndexes[0]...visibleIndexes[visibleIndexes.count - 1])
+            proceedDeletionInRange(visibleIndexes[0]...visibleIndexes[visibleIndexes.count - 1], animated: animated)
         }
         currentCardIndex -= Array(indexRange).filter { $0 < currentCardIndex }.count
         loadMissingCards(missingCardsCount())
